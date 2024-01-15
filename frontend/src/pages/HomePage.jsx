6f92d12c4e4b1ca874240b5949307e1cab7a17c5
Hitesh <h08rajbhar@gmail.com>
2024-01-15 18:22:51 +0530

    get products from backend to homepage fron redux toolkit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,28 @@
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
+import { useGetProductsQuery } from '../redux/api/productApi';
 import Product from '../components/Product';
 
 const HomePage = () => {
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
     <>
-      <h1 className="text-center">Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <>Loading</>
+      ) : error ? (
+        <>{error.data.message || error.error}</>
+      ) : (
+        <>
+          <h1 className="text-center">Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
