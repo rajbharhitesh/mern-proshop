bb59f39cbfece2f7d709b762b7d8f905b5cbb3ec
Hitesh <h08rajbhar@gmail.com>
2024-01-17 18:16:40 +0530

    create routes and controller for getting and updating profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,3 +78,64 @@ const logoutUser = (req, res) => {
   });
   res.status(200).json({ message: 'Logged out successfully' });
 };
+
+/**-----------------------------------------------
+ * @desc     Get user profile
+ * @route   /api/users/profile
+ * @method  GET
+ * @access  Private
+ ------------------------------------------------*/
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+/**-----------------------------------------------
+ * @desc     Update user profile
+ * @route   /api/users/profile
+ * @method  PUT
+ * @access  Private
+ ------------------------------------------------*/
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,12 +3,20 @@ import {
   authUser,
   registerUser,
   logoutUser,
+  getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
+
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
