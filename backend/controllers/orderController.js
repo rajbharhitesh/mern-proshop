b793b8968c9f55598a91bb311fd8e2135ce95513
Hitesh <h08rajbhar@gmail.com>
2024-01-18 14:12:10 +0530

    order controller for create get and get by id  and order route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,41 @@ import Order from '../models/orderModel.js';
  * @method  POST
  * @access  Private
  ------------------------------------------------*/
-const addOrderItems = asyncHandler(async (req, res) => {});
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
 
 /**-----------------------------------------------
  * @desc    Get logged in users orders
@@ -15,7 +49,10 @@ const addOrderItems = asyncHandler(async (req, res) => {});
  * @method  GET
  * @access  Private
  ------------------------------------------------*/
-const getMyOrders = asyncHandler(async (req, res) => {});
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
 
 /**-----------------------------------------------
  * @desc    Get order by id
@@ -23,7 +60,19 @@ const getMyOrders = asyncHandler(async (req, res) => {});
  * @method  GET
  * @access  Private
  ------------------------------------------------*/
-const getOrderById = asyncHandler(async (req, res) => {});
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
 
 /**-----------------------------------------------
  * @desc    Update order to paid
