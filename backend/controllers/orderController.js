a7812b7b7585178374890fbbfa2bf94143ab91c9
Hitesh <h08rajbhar@gmail.com>
2024-01-18 15:37:39 +0530

    update order to paid and create paypal client id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,15 +77,35 @@ const getOrderById = asyncHandler(async (req, res) => {
 /**-----------------------------------------------
  * @desc    Update order to paid
  * @route   /api/orders/:id/pay
- * @method  GET
+ * @method  PUT
  * @access  Private
  ------------------------------------------------*/
-const updateOrderToPaid = asyncHandler(async (req, res) => {});
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
 
 /**-----------------------------------------------
  * @desc    Update order to deliver
  * @route   /api/orders/:id/deliver
- * @method  GET
+ * @method  PUT
  * @access  Private  --- ADMIN
  ------------------------------------------------*/
 const updateOrderToDeliver = asyncHandler(async (req, res) => {});
--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,10 @@ app.use('/api/products', productRoute);
 app.use('/api/users', userRoute);
 app.use('/api/orders', orderRoute);
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
 app.use(notFound);
 app.use(errorHandler);
 
