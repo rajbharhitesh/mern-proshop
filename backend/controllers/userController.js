03ddfe44fc4985d977b2b44977f8c729642c4976
Hitesh <h08rajbhar@gmail.com>
2024-01-19 14:31:21 +0530

    admin routes for update get getsingleuser delete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -132,10 +132,93 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+/**-----------------------------------------------
+ * @desc     Get Users
+ * @route   /api/users
+ * @method  GET
+ * @access  Private --- ADMIN
+ ------------------------------------------------*/
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+/**-----------------------------------------------
+ * @desc     Delete User
+ * @route   /api/users/:id
+ * @method  DELETE
+ * @access  Private --- ADMIN
+ ------------------------------------------------*/
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error('Can not delete admin user');
+    }
+
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+/**-----------------------------------------------
+ * @desc     Get single User
+ * @route   /api/users/:id
+ * @method  GET
+ * @access  Private --- ADMIN
+ ------------------------------------------------*/
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+/**-----------------------------------------------
+ * @desc     Update User
+ * @route   /api/users/:id
+ * @method  PUT
+ * @access  Private --- ADMIN
+ ------------------------------------------------*/
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   registerUser,
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,12 +5,16 @@ import {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 
@@ -18,5 +22,11 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
