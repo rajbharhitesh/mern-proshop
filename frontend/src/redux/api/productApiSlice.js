d3c3dff0fb3aa6915db6d1ec56d68b72523173c5
Hitesh <h08rajbhar@gmail.com>
2024-01-19 16:22:35 +0530

    create review on  product functionality

--- frontend/src/redux/api/productApiSlice.js
+++ frontend/src/redux/api/productApiSlice.js
@@ -50,6 +50,15 @@ export const productApiSlice = apiSlice.injectEndpoints({
       }),
       providesTags: ['Product'],
     }),
+
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Product'],
+    }),
   }),
 });
 
@@ -60,4 +69,5 @@ export const {
   useUpdateProductMutation,
   useUploadProductImageMutation,
   useDeleteProductMutation,
+  useCreateReviewMutation,
 } = productApiSlice;
